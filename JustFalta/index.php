dd639620249462eab4bcd4548bfb3321206bc217
Ewerton Petillo <ewerton.petillo@gmail.com>
2019-08-27 08:42:40 -0400

    Add files via upload

---
+++ JustFalta/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../images/logo.png">
+
+    <title>Justificativas de Faltas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="index_arquivos/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="index_arquivos/floating-labels.css" rel="stylesheet">
+  </head>
+
+  <body background="images/bg.png">
+        <form class="form-signin"  action= "html/login.php" method="post" >
+        	<div class="text-center mb-4" >
+            <img class="mb-4" src="images/logo.png" alt="" width="108" height="108">
+            <h3>CONTROLE DE JUSTIFICATIVAS DE FALTAS</h3>
+            <p>Login</p>
+          </div>
+  	      <div class="form-label-group">
+  	        <input id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" type="email" 
+            name="login">
+  	        <label for="inputEmail">Email</label>
+  	      </div>
+
+  	      <div class="form-label-group">
+  	        <input id="inputPassword" class="form-control" placeholder="Password" required="" type="password" name="senha">
+  	        <label for="inputPassword">Senha</label>
+  	      </div>
+  	            <div class="checkbox mb-3">
+          <label>
+            <input value="remember-me" type="checkbox"> Lembrar-me
+          </label>
+	            <button class="btn btn-lg btn-primary btn-block" type="submit" >Login</button>
+      <p class="mt-5 mb-3 text-muted text-center">Desenvolvido por Ewerton Petillo <br> © 2019</p>
+	    </form>
+      </div>
+  
+  
+
+</body></html>
